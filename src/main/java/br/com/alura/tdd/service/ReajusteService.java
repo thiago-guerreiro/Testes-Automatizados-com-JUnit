4513223a6fdb23b2ac50cbbf3d2824ce627230c0
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2022-11-21 21:44:59 -0300

    Refatoração da método concederReajuste e enum Desempenho

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -6,18 +6,12 @@ import br.com.alura.tdd.modelo.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteService {
-
-	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-		if(desempenho == Desempenho.A_DESEJAR) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-			funcionario.reajustarSalario(reajuste);
-		} else if (desempenho == Desempenho.BOM) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-			funcionario.reajustarSalario(reajuste);
-		} else {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.2"));
-			funcionario.reajustarSalario(reajuste);
-		}
+	
+	public void concederReajuste(Funcionario funcionario,
+			Desempenho desempenho) {
+		BigDecimal reajuste = desempenho.percentualReajuste();
+		funcionario.reajustarSalario(reajuste);
 	}
+
 
 }
