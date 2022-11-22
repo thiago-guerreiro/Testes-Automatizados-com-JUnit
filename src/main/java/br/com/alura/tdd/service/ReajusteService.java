9021eb296d9b855b8668fff885227ef38e27e7f8
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2022-11-21 21:12:39 -0300

    classes ReajusteServiceTest e ReajusteService

---
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -0,0 +1,12 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class ReajusteService {
+
+	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
+		
+	}
+
+}
